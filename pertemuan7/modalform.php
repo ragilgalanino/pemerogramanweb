f9a66f0a95357811e860f702bebac6ab15a70b71
unknown <ragilgalanino@gmail.com>
2020-10-27 12:01:06 +0700

    pertemuan 7

---
+++ pertemuan7/modalform.php
@@ -0,0 +1,39 @@
+<!-- Ini Modal -->
+ <div class="modal" id="pesan">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <!-- ini header -->
+                    <div class="modal-header">
+                        <h4 class="modal-title">Konfirmasi Pendaftaran</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <!-- body -->
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-6"><b>Kode Mata Kuliah</b></div>
+                                <div class="col-6"><span id="kdmk"></span></div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6"><b>Mata Kuliah</b></div>
+                                <div class="col-6"><span id="nmmk"></span></div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6"><b>Kategori</b></div>
+                                <div class="col-6"><span id="kat"></span></div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6"><b>SKS</b></div>
+                                <div class="col-6"><span id="sksmk"></span> sks</div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- footer -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-info" data-dismiss="modal">Edit</button>
+                        <button id="proses" type="button" class="btn btn-primary" data-dismiss="modal">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!-- ini end modal -->
