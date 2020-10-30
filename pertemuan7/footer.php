67a7a46f2bf8110e95073e543fc41c2efb424cd5
unknown <ragilgalanino@gmail.com>
2020-10-30 16:29:20 +0700

    delete pertemuan7

--- pertemuan7/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="container-fluid bg-dark text-white text-center p-3 m-2">
-    <div class="row">
-        <div class="col-md-12">
-            Copyright &copy Aplikasi XYZ 2020
-        </div>
-    </div>
-</div>
